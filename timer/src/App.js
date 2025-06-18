4af3ae67ef383db37ff2bf55d12a2f95ef23a765
tlsgud0 <tlsgudpark1930@gmail.com>
2025-06-19 00:40:14 +0900

    버튼구현

--- timer/src/App.js
+++ timer/src/App.js
@@ -4,13 +4,21 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <head>
-        <title>365 뽀모도로</title>
-      </head>
-      <header className="App-header">
-        <p className="header-left">365 뽀모도로</p>
+      {/* 헤더 */}
+      <header className="header">
+        <h1 className="title">365 뽀모도로</h1>
         <p className="mode-toggle">Light | Dark</p>
       </header>
+
+      {/* 타이머 영역 (추후 구현) */}
+      <div className="timer-area"></div>
+
+      {/* 버튼 그룹 */}
+      <div className="buttons">
+        <button className="btn start">시작</button>
+        <button className="btn stop">정지</button>
+        <button className="btn reset">초기화</button>
+      </div>
     </div>
   );
 }
